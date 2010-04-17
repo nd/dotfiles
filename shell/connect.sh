d270f48aeca03cca4ccae0696535d2573b335fc9
nd <nd@lend.(none)>
2010-04-17 10:30:03 +0400

    remove old stuff

--- shell/connect.sh
+++
@@ -1,6 +0,0 @@
-sudo ifconfig ra0 up							 
-sudo iwpriv ra0 set EncrypType=WEP 
-sudo iwpriv ra0 set Key1=EFEFEFEFEF
-sudo iwpriv ra0 set DefaultKeyID=1 
-sudo iwpriv ra0 set SSID=mojacet	 
-dhclient ra0											 
